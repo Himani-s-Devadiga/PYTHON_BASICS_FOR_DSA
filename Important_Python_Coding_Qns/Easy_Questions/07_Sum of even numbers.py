e51a976b6696f85175f686ce400c85ce4e5a0100
Himani-S-Devadiga <himanisdevadiga59@gmail.com>
2026-09-01 06:44:37 +0530

    Create 07_Sum of even numbers.py

---
+++ Important_Python_Coding_Qns/Easy_Questions/07_Sum of even numbers.py
@@ -0,0 +1,19 @@
+N=int(input("enter a number"))
+i=1
+total=0
+while i<=N:
+     if i%2==0:
+       total=total+i
+      i=i+1
+
+print(total)       
+
+
+### Note:
+🔥 This is an important DSA lesson
+When using a while loop:
+while condition:
+    # work
+
+    # update counter
+       
