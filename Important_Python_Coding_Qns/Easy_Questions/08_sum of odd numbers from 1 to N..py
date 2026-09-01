ad15824c9a1394de01ae7e357b1d9fe291a4f30e
Himani-S-Devadiga <himanisdevadiga59@gmail.com>
2026-09-01 06:48:09 +0530

    Create 08_sum of odd numbers from 1 to N..py

---
+++ Important_Python_Coding_Qns/Easy_Questions/08_sum of odd numbers from 1 to N..py
@@ -0,0 +1,16 @@
+N=int(input("enter the sum of numbers"))
+odd=0
+i=1
+while i<=N:
+      if i%2!=0:
+         odd=odd+i
+      i=i+1
+
+print(odd)
+
+
+### Note:
+while condition:
+    if condition:
+        # do something
+    i = i + 1
